0055a558ee84f481b78dc9084fe5aaba265a35e7
kimbasn <kimbasabingoye@gmail.com>
2025-03-13 10:40:47 +0100

    Remove header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { CanvasComponent } from "./components/Canvas/Canvas";
-import { Header } from "./components/Common/Header";
-
 
 function App() {
   return (
     <>
-      <Header />
       <CanvasComponent />
     </>
 
